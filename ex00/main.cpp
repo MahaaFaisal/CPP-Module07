a2ffdfee26f339cd4e6cb35e6277f7cb88546086
mahaafaisal <mahafaisalmubarak@gmail.com>
2024-10-07 19:00:20 +0400

    merged mains

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include "whatever.hpp"
 
+class Awesome
+{
+	public:
+		Awesome(void) : _n(0) {}
+		Awesome( int n ) : _n( n ) {}
+		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+		int get_n() const { return _n; }
+
+	private:
+		int _n;
+};
+
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+
 int main( void )
 {
 	int a = 2;
@@ -17,5 +38,12 @@ int main( void )
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min<std::string>( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max<std::string>( c, d ) << std::endl;
+
+	Awesome a1(2), b1(4);
+
+	swap(a1, b1);
+	std::cout << a1 << " " << b1 << std::endl;
+	std::cout << max(a1, b1) << std::endl;
+	std::cout << min(a1, b1) << std::endl;
 	return 0;
 }
